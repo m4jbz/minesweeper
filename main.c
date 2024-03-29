9fb97155feb654c3a40845cc80c7875f3b9ab114
m4jbz <zeroknel@proton.me>
2024-03-28 20:33:11 -0600

    corners and sides possible positions, int to char

--- main.c
+++ main.c
@@ -3,47 +3,63 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define PLACES 64
+#define POSITIONS 64
 #define MINES 10
+// Colors
 #define RED "\033[31m"
+#define BLUE "\033[34m"
 #define GREEN "\033[32m"
 #define RESET "\033[0m"
 
 int *minePositions();
-int *boardMaker(int *board);
-void printBoard(int *board);
-void resetBoard(int *board);
+int possiblePos(char *board);
+char *boardMaker(char *board);
+void printBoard(char *board);
+void resetBoard(char *board);
 void cleanTerm();
 
 int main()
 {
-	int *board = calloc(PLACES, sizeof(int));
-	int boardNumber = 0;
+	char *board = calloc(POSITIONS, sizeof(int));
+	resetBoard(board);
 
 	while (1)
 	{
 		cleanTerm();
-		printf("Board N° %d\n", boardNumber);
 		printBoard(boardMaker(board));
 		resetBoard(board);
-		boardNumber++;
 		sleep(2);
 	}
 	return 0;
+	free(board);
 }
 
-int *boardMaker(int *board)
+char *boardMaker(char *board)
 {
 	int *numbers = minePositions();
 
 	for (int i = 0; i < 10; i++)
-	{
-		board[numbers[i]] = 1;
-	}
+		board[numbers[i]] = 'Q';
 
 	return board;
-
 	free(numbers);
+}
+
+int possiblePos(char *board)
+{
+	for (int i = 0; i < POSITIONS; i++)
+	{
+		if (board[i] != 'Q')
+		{
+			if (i == 0 || i == 7 || i == 56 || i == 63)
+				return 3;
+			else if (i % 8 == 0 || i % 8 == 7 || i < 8 || i > 55)
+				return 5;
+			else
+				return 8;
+		}
+	}
+	return 0;
 }
 
 int* minePositions()
@@ -74,7 +90,7 @@ int* minePositions()
 	free(array);
 }
 
-void printBoard(int *board) 
+void printBoard(char *board) 
 {
 	int k = 0;
 
@@ -84,10 +100,12 @@ void printBoard(int *board)
 		printf("%*s|", 55, "");
 		for (int j = 0; j < 8; j++)
 		{
-			if (board[k] == 0)
-				printf(" %s%d %s", RED, board[k], RESET);
+			if (board[k] == ' ')
+				printf(" %s%c %s", RED, board[k], RESET);
+			else if (board[k] == 'Q')
+				printf(" %s%c %s", GREEN, board[k], RESET);
 			else 
-				printf(" %s%d %s", GREEN, board[k], RESET);
+				printf(" %s%c %s", BLUE, board[k], RESET);
 			printf("|");
 			k++;
 		}
@@ -96,10 +114,9 @@ void printBoard(int *board)
 	}
 }
 
-void resetBoard(int *board) {
-	for (int i = 0; i < PLACES; i++) {
-		board[i] = 0;
-	}
+void resetBoard(char *board) {
+	for (int i = 0; i < POSITIONS; i++)
+		board[i] = ' ';
 }
 
 void cleanTerm()
